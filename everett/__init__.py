2a7b1e6252088db2c9b0e82a78ef7ff93c8988e7
Will Kahn-Greene <willkg@mozilla.com>
2016-08-01 18:19:13 -0400

    Update version for development

--- everett/__init__.py
+++ everett/__init__.py
@@ -15,6 +15,6 @@ __author__ = 'Will Kahn-Greene'
 __email__ = 'willkg@mozilla.com'
 
 # yyyymmdd
-__releasedate__ = '20160801'
+__releasedate__ = ''
 # x.y or x.y.dev
-__version__ = '0.1'
+__version__ = '0.2.dev'
