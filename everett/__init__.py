da7def8108cf025c07ead579fdf30d17f9479b91
Will Kahn-Greene <willkg@mozilla.com>
2019-01-09 16:15:25 -0500

    Update for 1.0.2 development

--- everett/__init__.py
+++ everett/__init__.py
@@ -7,9 +7,9 @@ __author__ = 'Will Kahn-Greene'
 __email__ = 'willkg@mozilla.com'
 
 # yyyymmdd
-__releasedate__ = '20190108'
+__releasedate__ = ''
 # x.y.z or x.y.z.dev0
-__version__ = '1.0.1'
+__version__ = '1.0.2.dev0'
 
 
 # NoValue instances are always false
