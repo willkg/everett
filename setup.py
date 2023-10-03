3b9638c9dd3a9f6175eed382067663fecdf7eb9d
Will Kahn-Greene <willkg@mozilla.com>
2023-10-03 11:07:24 -0400

    Add support for Python 3.12 (#221)

--- setup.py
+++ setup.py
@@ -56,6 +56,9 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     project_urls={
         "Documentation": "https://everett.readthedocs.io/",
